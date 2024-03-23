bf0dd3f7b22c0ce580d94c1ab01ae9346fd74e5f
kennedy <kennedywakaba5@gmail.com>
2024-03-23 20:29:01 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+    const headquarters = 42;
+    const blocks = (pickup - headquarters);
+    return blocks;  
+}
+function distanceFromHqInFeet(pickup) {
+    distanceFromHqInBlocks(pickup);
+    const blocks = distanceFromHqInBlocks(pickup);
+    const distanceInFeet = blocks * 264;
+    return distanceInFeet;
+}
+function distanceTravelledInFeet(pickup) {
+    distanceFromHqInBlocks(pickup);
+    const blocks = distanceFromHqInBlocks(pickup);
+    const distanceInFeet = blocks * 264;
+    return distanceInFeet;  
+}
+function calculatesFarePrice(pickup) {
+    const distanceInFeet =  distanceTravelledInFeet(pickup);
+    if (distanceInFeet <= 400) {
+     return 0;   
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+        return (distanceInFeet - 400) * 0.02 
+    } else if (distanceInFeet > 2000 && distanceInFeet < 2500) {
+       return 25; 
+    } else {
+        return 'cannot travel that far';  
+    } 
+}
